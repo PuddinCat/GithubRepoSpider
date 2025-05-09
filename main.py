37b76209f7115a6098139ee525f0cddcbbee03f1
PuddinCat <131779933+PuddinCat@users.noreply.github.com>
2025-05-09 21:23:25 +0800

    Async search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import asyncio
 
 import jsonschema
@@ -13,32 +13,61 @@ GITHUB_TOKEN = os.environ["GITHUB_API_TOKEN"]
 GITHUB_API_URL = "https://api.github.com/search/repositories"
 
 
-async def search_github_repositories(query, sort="stars", order="desc"):
+async def search_github_repositories(query, sort="stars", order="desc", pages=5):
     headers = {
         "Authorization": f"token {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3+json",
     }
-    params = {"q": query, "sort": sort, "order": order}
+    all_results = {"items": []}
     async with httpx.AsyncClient() as client:
-        resp = await client.get(GITHUB_API_URL, headers=headers, params=params)
-        if resp.status_code == 200:
+        resps = await asyncio.gather(
+            *[
+                client.get(
+                    GITHUB_API_URL,
+                    headers=headers,
+                    params={
+                        "q": query,
+                        "sort": sort,
+                        "order": order,
+                        "page": page,
+                        "per_page": 30,
+                    },
+                )
+                for page in range(1, pages + 1)
+            ]
+        )
+        all_results = []
+        for page, resp in zip(range(1, pages + 1), resps):
+            if resp.status_code != 200:
+                print(f"Error on page {page}: {resp.status_code}, {resp.text}")
+                return None
+
             data = resp.json()
             try:
                 jsonschema.validate(instance=data, schema=JSON_SCHEMA)
-                return data
+                all_results += data["items"]
             except jsonschema.ValidationError as e:
-                print(f"JSON Schema validation error: {e}")
+                print(f"JSON Schema validation error on page {page}: {e}")
                 return None
-        else:
-            print(f"Error: {resp.status_code}, {resp.text}")
-            return None
+    return all_results
 
 
 async def main():
-    query = input("Enter search query: ")
-    results = await search_github_repositories(query)
-    if results:
-        for repo in results["items"]:
+    keywords = [
+        "CVE-2025",
+    ]
+    created_time_since = (datetime.now() - timedelta(days=2)).strftime("%Y-%m-%d")
+    search_results = await asyncio.gather(
+        *[
+            search_github_repositories(f"{keyword} created:>{created_time_since}")
+            for keyword in keywords
+        ]
+    )
+    for keyword, repos in zip(keywords, search_results):
+        if not repos:
+            print(f"Search {keyword} failed")
+            continue
+        for repo in repos:
             created_at = datetime.strptime(repo["created_at"], "%Y-%m-%dT%H:%M:%SZ")
             print(
                 f"Name: {repo['name']}, Author: {repo['owner']['login']}, "
